7dabb30887bd88ef81e54890ce51a6a85c7c6fb3
NoN4ame <Keir548@yandex.ru>
2021-01-21 20:38:27 +0300

    Сделал добавление товара в корзину

--- js/cart.js
+++ js/cart.js
@@ -5,15 +5,16 @@ document.getElementById('cart').addEventListener('click', () => {
     document.querySelector('main').insertAdjacentHTML('beforeend',
         `<div class="invisible">
                     <div class="cart">
+                    <h1>КОРЗИНА</h1>
+                    <img class="close" src="../img/other/close.svg" alt="close">
+                    <hr>
                   </div>
             </div>`)
     // Проверяем, если корзина пуста, то выводим пустой шаблон
     if (cart.length === 0) {
         document.querySelector('.cart').insertAdjacentHTML('beforeend',
-            `<h1>КОРЗИНА</h1>
-                    <img class="close" src="../img/other/close.svg" alt="close">
-                    <hr>
-                    <p>Корзина Пуста</p>
+            `
+                    <p class="empty-cart">Корзина Пуста</p>
                     <a href="../templates/catalog.html">
                         <button>В каталог</button>
                     </a>
@@ -23,11 +24,48 @@ document.getElementById('cart').addEventListener('click', () => {
         // Вызов событий для закрывания окна корзины
         this.triggers()
         // Если корзина не пуста, то выводим товары в корзине
-    } else cartItems()
+    } else if (cart.length > 0) {
+        renderCartProduct(cart)
+        this.styleCard()
+        this.triggers()
+        document.querySelector('.cart').insertAdjacentHTML('beforeend',
+            `<hr>
+            <div>
+                <section class="promo-code">
+                    <input type="text" placeholder="Промокод">
+                </section>
+                <section class="result">
+                    <section><p>Скидка: <p class="sales"></p></p></section>
+                    <section><p>Промокод: <p class="promo-code-result"></p></p></section>
+                    <section><p>К ОПЛАТЕ: <p class="total-price"></p></p></section>
+                    <button>ПЕРЕЙТИ К ОФОРМЛЕНИЮ ЗАКАЗА</button>
+                </section>
+            </div>
+        `)
+    }
+
 })
 let cartItems = (img, name, vendorCode, price) => {
-    document.querySelector('.cart').insertAdjacentHTML('beforeend',
-        `<div>123</div>`)
+   return `   <div class="cart-item">
+               <img class="item-img" src="${img}" alt="product">
+                <ul>
+                    <li class="cart-item__name">${name}</li>
+                    <li class="cart-item__vendorCode">${vendorCode}</li>
+                </ul>
+               <section class="cart-item__quantity">
+                    <span id="plus">+</span>
+                    <p class="quantity-result">1</p>
+                    <span id="minus">-</span>
+               </section>
+               <p class="cart-item__price">${price}</p>
+               <span>
+                     <img class="cart-item__delete" src="../img/other/del_product.svg" alt="del">
+               </span>
+           </div>`
+}
+const renderCartProduct = list => {
+    const productList = list.map(item => cartItems(item.img,item.name,item.vendorCode,item.price));
+    document.querySelector('.cart').insertAdjacentHTML('beforeend', productList.join(''))
 }
 function styleCard() {
     document.querySelector('body').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
