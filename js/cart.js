cd479e4f0494b822f32588382d82f3763319c175
NoN4ame <Keir548@yandex.ru>
2021-02-03 19:16:12 +0300

    пересбор проекта

--- js/cart.js
+++ js/cart.js
@@ -10,62 +10,33 @@ document.getElementById('cart').addEventListener('click', () => {
                     <img class="close" src="../img/other/close.svg" alt="close">
                   </div>
             </div>`)
-    // Проверяем, если корзина пуста, то выводим пустой шаблон
     if (cart.length === 0) {
-        // Вывод шаблона пустой корзины
         emptyCart()
-        // Вызов событий для закрывания окна корзины
         triggers()
-        // Если корзина не пуста, то выводим товары в корзине
-    } else _render()
+    } else document.querySelector('.cart').insertAdjacentHTML('beforeend', renderCart)
 })
-// Шаблон корзины
-let cartItems = (img, name, quantity, vendorCode, price) => {
-    return `   <div class="cart-item">
-               <img class="item-img" src="${img}" alt="product">
+const renderCart = () => {
+    for (let i = 0; i < cart.length; i++) {
+        `<div class="cart-item">
+               <img class="item-img" src="${cart[i].img}" alt="product">
                 <ul>
-                    <li class="cart-item__name">${name}</li>
-                    <li class="cart-item__vendorCode">${vendorCode}</li>
+                    <li class="cart-item__name">${cart[i].name}</li>
+                    <li class="cart-item__vendorCode">${cart[i].vendorCode}</li>
                 </ul>
                <section class="cart-item__quantity">
                     <span class="minus">-</span>
-                    <p class="quantity-result">${quantity}</p>
+                    <p class="quantity-result">${cart[i].quantity}</p>
                     <span class="plus">+</span>
                </section>
                <section class="sales"></section>
-               <p class="cart-item__price">${price}</p>
+               <p class="cart-item__price">${cart[i].price}</p>
                <span class="cart-item__delete">
                <p><span class="delete">+</span></p>
                </span>
                </div>`
-}
-const renderCartProduct = list => {
-    const productList = list.map(item => cartItems(item.img, item.name, item.quantity, item.vendorCode, item.price));
-    document.querySelector('.cart').insertAdjacentHTML('beforeend', productList.join(''))
-}
-
-// Работа с корзиной
-function quantity() {
-    let cartItem = document.querySelectorAll('.cart-item')
-    let plus = document.querySelectorAll('.plus')
-    let minus = document.querySelectorAll('.minus')
-    for (let i = 0; i < cart.length; i++){
-        cartItem[i].addEventListener('click', (e) => {
-            if (e.target === plus[i] && cart[i].quantity < 4){
-                cart[i].quantity++
-                document.querySelectorAll('.quantity-result')[i].innerHTML = cart[i].quantity
-            } else if (e.target === minus[i] && cart[i].quantity >= 1){
-                cart[i].quantity--
-                document.querySelectorAll('.quantity-result')[i].innerHTML = cart[i].quantity
-                if (cart[i].quantity === 0){
-                    cart.splice([i],1)
-                    cartItem[i].remove()
-                }
-            }
-        })
     }
 }
-// Вывод пустой корзины
+// Корзина пуста
 const emptyCart = () => {
     document.querySelector('.cart').insertAdjacentHTML('beforeend',
         `<p class="empty-cart">Корзина Пуста</p>
@@ -73,13 +44,7 @@ const emptyCart = () => {
                                               <button>В каталог</button>
                                           </a>`)
 }
-// Стили заднего фона при открытой корзине
-function _render (){
-    renderCartProduct(cart)
-    triggers()
-    totalPrice()
-    quantity()
-}
+
 
 function styleCard() {
     document.querySelector('body').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
@@ -88,7 +53,7 @@ function styleCard() {
         .forEach(div => div.style.filter = 'brightness(0.5)')
     document.querySelectorAll('.container')[0].style.filter = 'none'
 }
-// События по закрытию корзины
+
 function triggers() {
     styleCard()
     // Получаем блок обертку
@@ -116,51 +81,3 @@ function triggers() {
         } else document.querySelector('.close').src = '../img/other/close.svg'
     })
 }
-// Промокод и скидка
-function promoCode() {
-    let codeValue = document.getElementById('promoCodeValue')
-    if (codeValue.value === promo[0]) {
-        let sum = 0;
-        let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
-        let discount = (totalSum * 0.2);
-        codeValue.value = promo[0]
-        document.querySelector('.promo-code-result').innerHTML = `${discount.toLocaleString()}  &#8381;`
-        document.querySelector('.total-price').innerHTML = `${totalSum - discount} &#8381; `
-    }
-    codeValue.addEventListener('keyup', () => {
-        if (codeValue.value === promo[0]) {
-            let sum = 0;
-            let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
-            let discount = (totalSum * 0.2);
-            document.querySelector('.promo-code-result').innerHTML = `${discount.toLocaleString()}  &#8381;`
-            document.querySelector('.total-price').innerHTML = `${totalSum - discount} &#8381; `
-            codeValue.innerHTML = promo[0]
-        } else return false
-    })
-}
-
-function totalPrice() {
-    // Нижняя часть корзины
-    let sum = 0;
-    let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
-    // Рендер нижней части корзины (Промокод, Цены)
-    document.querySelector('.cart').insertAdjacentHTML('beforeend',
-        `<div class="cart-bottom">
-                <section class="promo-code">
-                    <input id="promoCodeValue" type="text" placeholder="Промокод" value="">
-                </section>
-                <section class="result">
-                    <section><p>Скидка: <p class="sales-result"></p></p></section>
-                    <section><p>Промокод: <p class="promo-code-result"></p></p></section>
-                    <section><p>К ОПЛАТЕ: <p class="total-price">${totalSum.toLocaleString()} &#8381;</p></p></section>
-                    <button>ПЕРЕЙТИ К ОФОРМЛЕНИЮ ЗАКАЗА</button>
-                </section>
-            </div>`)
-    // Вызов метода, промокода и скидок
-    promoCode()
-}
-
-// НОВАЯ ВЕТКА
-
-
-
---
+++ js/other.js
@@ -0,0 +1,142 @@
+let cartItems = (img, name, quantity, vendorCode, price) => {
+    return `   <div class="cart-item">
+               <img class="item-img" src="${img}" alt="product">
+                <ul>
+                    <li class="cart-item__name">${name}</li>
+                    <li class="cart-item__vendorCode">${vendorCode}</li>
+                </ul>
+               <section class="cart-item__quantity">
+                    <span class="minus">-</span>
+                    <p class="quantity-result">${quantity}</p>
+                    <span class="plus">+</span>
+               </section>
+               <section class="sales"></section>
+               <p class="cart-item__price">${price}</p>
+               <span class="cart-item__delete">
+               <p><span class="delete">+</span></p>
+               </span>
+               </div>`
+}
+const renderCartProduct = list => {
+    const productList = list.map(item => cartItems(item.img, item.name, item.quantity, item.vendorCode, item.price));
+    document.querySelector('.cart').insertAdjacentHTML('beforeend', productList.join(''))
+}
+
+// Работа с корзиной
+function quantity() {
+    let plus = document.querySelectorAll('.plus')
+    let minus = document.querySelectorAll('.minus')
+    for (let i = 0; i < cart.length; i++){
+        let cartItem = document.querySelectorAll('.cart-item')
+        test(cartItem)
+        cartItem[i].addEventListener('click', (e) => {
+            if (e.target === plus[i] && cart[i].quantity < 4){
+                cart[i].quantity++
+                document.querySelectorAll('.quantity-result')[i].innerHTML = cart[i].quantity
+            } else if (e.target === minus[i] && cart[i].quantity >= 1){
+                cart[i].quantity--
+                document.querySelectorAll('.quantity-result')[i].innerHTML = cart[i].quantity
+                if (cart[i].quantity === 0){
+                    delete cart[i]
+                    cartItem[i].remove()
+                }
+            }
+        })
+    }
+}
+// Вывод пустой корзины
+const emptyCart = () => {
+    document.querySelector('.cart').insertAdjacentHTML('beforeend',
+        `<p class="empty-cart">Корзина Пуста</p>
+                                          <a href="../templates/catalog.html">
+                                              <button>В каталог</button>
+                                          </a>`)
+}
+// Стили заднего фона при открытой корзине
+function _render (){
+    renderCartProduct(cart)
+    triggers()
+    totalPrice()
+    quantity()
+}
+
+function styleCard() {
+    document.querySelector('body').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+    document.querySelector('header').style.filter = 'brightness(0.5)'
+    document.querySelectorAll('.container')
+        .forEach(div => div.style.filter = 'brightness(0.5)')
+    document.querySelectorAll('.container')[0].style.filter = 'none'
+}
+// События по закрытию корзины
+function triggers() {
+    styleCard()
+    // Получаем блок обертку
+    let card = document.querySelector('.invisible');
+    // Вешаем на него обработчик событий
+    card.addEventListener('click', (e) => {
+        // Если клик был по блоку или на иконку 'X', то закрываем карточку товара
+        if (e.target === document.querySelector('.close')
+            || e.target === document.querySelector('.invisible')) {
+            document.querySelector('.cart').remove()
+            document.querySelector('.invisible').remove()
+            document.querySelector('body').style.backgroundColor = 'transparent'
+            document.querySelector('header').style.filter = 'none'
+            document.querySelectorAll('.container')
+                .forEach(div => div.style.filter = 'none')
+        }
+    })
+    // Вешаем обработчик при наведении
+    card.addEventListener('mouseover', (e) => {
+        // Если наведение было по иконке 'X'
+        if (e.target === document.querySelector('.close')) {
+            // Меняем ее на иконку розового цвета
+            document.querySelector('.close').src = '../img/other/close-pink.svg'
+            // Если курсор не на иконке, то оставляем стандартную иконку
+        } else document.querySelector('.close').src = '../img/other/close.svg'
+    })
+}
+// Промокод и скидка
+function promoCode() {
+    let codeValue = document.getElementById('promoCodeValue')
+    if (codeValue.value === promo[0]) {
+        let sum = 0;
+        let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
+        let discount = (totalSum * 0.2);
+        codeValue.value = promo[0]
+        document.querySelector('.promo-code-result').innerHTML = `${discount.toLocaleString()}  &#8381;`
+        document.querySelector('.total-price').innerHTML = `${totalSum - discount} &#8381; `
+    }
+    codeValue.addEventListener('keyup', () => {
+        if (codeValue.value === promo[0]) {
+            let sum = 0;
+            let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
+            let discount = (totalSum * 0.2);
+            document.querySelector('.promo-code-result').innerHTML = `${discount.toLocaleString()}  &#8381;`
+            document.querySelector('.total-price').innerHTML = `${totalSum - discount} &#8381; `
+            codeValue.innerHTML = promo[0]
+        } else return false
+    })
+}
+
+function totalPrice() {
+    // Нижняя часть корзины
+    let sum = 0;
+    let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
+    // Рендер нижней части корзины (Промокод, Цены)
+    document.querySelector('.cart').insertAdjacentHTML('beforeend',
+        `<div class="cart-bottom">
+                <section class="promo-code">
+                    <input id="promoCodeValue" type="text" placeholder="Промокод" value="">
+                </section>
+                <section class="result">
+                    <section><p>Скидка: <p class="sales-result"></p></p></section>
+                    <section><p>Промокод: <p class="promo-code-result"></p></p></section>
+                    <section><p>К ОПЛАТЕ: <p class="total-price">${totalSum.toLocaleString()} &#8381;</p></p></section>
+                    <button>ПЕРЕЙТИ К ОФОРМЛЕНИЮ ЗАКАЗА</button>
+                </section>
+            </div>`)
+    // Вызов метода, промокода и скидок
+    promoCode()
+}
+
+// НОВАЯ ВЕТКА
