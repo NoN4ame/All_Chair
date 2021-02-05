06ec3d31283c180d855b4dcbd323b7a1fd19c263
NoN4ame <Keir548@yandex.ru>
2021-02-05 15:38:09 +0300

    Корзина полностью готова

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,5 @@
 const cart = [];
-const promo = ['2021'];
+const promo = ['2021', '2020'];
 // Вешаем обработчик на элемент "Корзина"
 document.getElementById('cart').addEventListener('click', () => {
     // При клике создаем шаблон для корзины
@@ -21,6 +21,7 @@ document.getElementById('cart').addEventListener('click', () => {
     quantity()
     delChoice()
 })
+// Подсчет кол-ва товаров и вывод суммы с учетом кол-ва
 const quantity = () => {
     let cartItem = document.querySelectorAll('.cart-item')
     cartItem.forEach(el => el.addEventListener('click', (e) => {
@@ -41,10 +42,10 @@ const quantity = () => {
             find.totalPrice = find.price * find.quantity
             // Нижняя часть корзины
             totalPrice()
-
         }
     }))
 }
+// При клике на кнопку удалить, выводим плашку для подтверждения удаления или возврата товара в корзину
 const delChoice = () => {
     let cartItem = document.querySelectorAll('.cart-item')
     cartItem.forEach(el => el.addEventListener('click', (e) => {
@@ -59,25 +60,34 @@ const delChoice = () => {
                         </div>
                     </div>`);
         }
+        // Вызываем метод удаления эл-та
         delItem(el)
     }))
 }
+// Удаление товара из корзины, возврат товара в корзину
 const delItem = (cartItem) => {
     let delBlock = document.querySelectorAll('.delete-block')
     delBlock.forEach(item => item.addEventListener('click', (e) => {
         let returnInCart = item.querySelector('.return_inCart')
         let delSubmit = item.querySelector('.delete_submit')
+        // Если клик по "Вернуть в корзину", то удаляем плашку с выбором
         if (e.target === returnInCart) {
             item.remove()
+        // Если по удалить
         } else if (e.target === delSubmit) {
+            // Удаляем товар сначала HTML
             cartItem.remove()
+            // Находим совпадение по имени товара в массиве
             let index = cart.findIndex(n => n.name === cartItem.querySelector('.cart-item__name').textContent)
             if (index !== -1) {
+                // Если нашли, то удаляем
                 cart.splice(index, 1)
             }
+            // После удаления меням кол-во товара в корзине и считаем общую стоимость
             let quantity = document.querySelector('.quantity');
             quantity.innerHTML = String(cart.length)
             totalPrice()
+            // Если в массиве ни чего нет, выводим шаблон пустой корзины
             if (cart.length === 0) {
                 cartItem.remove()
                 quantity.remove()
@@ -86,6 +96,7 @@ const delItem = (cartItem) => {
             }
         }
     }))
+    scroll()
 }
 // Отрисовка шаблона
 const renderCart = () => {
@@ -108,6 +119,7 @@ const renderCart = () => {
                </span>
                </div>`)
     }
+    scroll ()
 }
 // Корзина пуста
 const emptyCart = () => {
@@ -117,21 +129,22 @@ const emptyCart = () => {
                                               <button>В каталог</button>
                                           </a>`)
 }
+//Подсчет общей суммы
 const totalPrice = () => {
     let sum = 0;
     let totalSum = cart.reduce((a,b) => a + b.totalPrice, sum);
     document.querySelector('.total-price').innerHTML = `${totalSum.toLocaleString()}  &#8381;`
 }
-// Общая сумма
+// Нижняя часть корзины
 function cartBottom() {
-    // Нижняя часть корзины
+    // Подсчет общей суммы товаров в массиве без учета кол-ва
     let sum = 0;
     let totalSum = cart.reduce((a,b) => a + b.totalPrice, sum);
     // Рендер нижней части корзины (Промокод, Цены)
     document.querySelector('.cart').insertAdjacentHTML('beforeend',
         `<div class="cart-bottom">
                 <section class="promo-code">
-                    <input id="promoCodeValue" type="text" placeholder="Промокод" value="">
+                    <input id="promoCodeValue" type="text" placeholder="Промокод" value="" maxlength="4">
                 </section>
                 <section class="result">
                     <section><p>Скидка: <p class="sales-result"></p></p></section>
@@ -143,28 +156,28 @@ function cartBottom() {
     promoCode()
     // Вызов метода, промокода и скидок
 }
+// Промокод для корзины
 function promoCode() {
     let codeValue = document.getElementById('promoCodeValue')
-    if (codeValue.value === promo[0]) {
-        let sum = 0;
-        let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
-        let discount = (totalSum * 0.2);
-        codeValue.value = promo[0]
-        document.querySelector('.promo-code-result').innerHTML = `${discount.toLocaleString()}  &#8381;`
-        document.querySelector('.total-price').innerHTML = `${totalSum - discount} &#8381; `
-    }
     codeValue.addEventListener('keyup', () => {
-        if (codeValue.value === promo[0]) {
+        // Ищем совпадение в массиве промокодов
+        if (promo.indexOf(codeValue.value) !== -1 && codeValue.value.length === 4) {
+            // Если нашли, то делаем расчет суммы с учетом промокода
             let sum = 0;
             let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
             let discount = (totalSum * 0.2);
-            document.querySelector('.promo-code-result').innerHTML = `${discount.toLocaleString()}  &#8381;`
-            document.querySelector('.total-price').innerHTML = `${totalSum - discount} &#8381; `
-            codeValue.innerHTML = promo[0]
-        } else return false
+            document.querySelector('.promo-code-result').innerHTML = `- ${discount.toLocaleString()}  &#8381;`
+            document.querySelector('.total-price').innerHTML = `${(totalSum - discount).toLocaleString()} &#8381; `
+            // Если совпадение нет, то возвращаем все как было
+        } else {
+            document.querySelector('.promo-code-result').innerHTML = ``
+            let sum = 0;
+            let totalSum = cart.reduce((a, b) => a + b.totalPrice, sum);
+            document.querySelector('.total-price').innerHTML = `${totalSum.toLocaleString()} &#8381; `
+        }
     })
 }
-
+// Стили заднего фона
 function styleCard() {
     document.querySelector('body').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
     document.querySelector('header').style.filter = 'brightness(0.5)'
@@ -172,6 +185,7 @@ function styleCard() {
         .forEach(div => div.style.filter = 'brightness(0.5)')
     document.querySelectorAll('.container')[0].style.filter = 'none'
 }
+// События по закрытию корзины и карточек товара
 function triggers() {
     styleCard()
     // Получаем блок обертку
@@ -198,4 +212,12 @@ function triggers() {
             // Если курсор не на иконке, то оставляем стандартную иконку
         } else document.querySelector('.close').src = '../img/other/close.svg'
     })
+}
+function scroll () {
+    if (cart.length >= 3) {
+        document.querySelector('.cart').style.overflowY = 'scroll'
+        document.querySelector('.cart').style.top = '3.5%'
+        document.querySelector('.cart').style.height = '775px'
+        document.querySelector('.cart').style.overflowX = 'none'
+    } else document.querySelector('.cart').style.height = 'auto'
 }
