850a4cb30bc9633b8168e718ac9bd1da83fcecb9
NoN4ame <Keir548@yandex.ru>
2021-02-15 14:40:41 +0300

    Доработки оформления заказа

--- js/ordering.js
+++ js/ordering.js
@@ -16,10 +16,10 @@ const ordering = {
                                 <section class="chooseDelivery">
                                 <ul>
                                     <li>
-                                        <input type="radio" name="delivery" id="delivery" value="Доставка" checked>
+                                        <input class="custom-radio" type="radio" name="delivery" id="delivery" value="Доставка">
                                         <label for="delivery">Доставка</label></li>
                                     <li>
-                                        <input type="radio" name="delivery" id="pickup" value="Самовывоз">
+                                        <input class="custom-radio" type="radio" name="delivery" id="pickup" value="Самовывоз">
                                         <label for="pickup">Самовывоз</label>
                                     </li>
                                 </ul>
@@ -33,29 +33,20 @@ const ordering = {
                                 </section>
                             </div>
                                 <div class="aboutRecipient">
-                                    <section class="recipient">
+                                     <section class="recipient">
                                     <p class="aboutRecipient__text">Данные получателя:</p>
                                     <ul>
                                     <li><input type="text" placeholder="Имя" name="contact" id="name"></li>
                                     <li><input type="number" placeholder="Телефон" name="contact" id="phone"></li>
                                     </ul>
                                     </section>
-                                    <section class="address">
-                                    <p class="aboutRecipient__text">Адрес получателя:</p>
-                                    <ul>
-                                    <li><input type="text" placeholder="Город" name="deliveryForm" id="city"></li>
-                                    <li><input type="text" placeholder="Улица" name="deliveryForm" id="street"></li>
-                                    <li><input type="text" placeholder="Дом" name="deliveryForm" id="houseNumber"></li>
-                                    <li><input type="text" placeholder="Квартира/офис" name="deliveryForm" id="apartment"></li>
-                                    </ul>
-                                    </section>
                                 </div>
                                 <div class="payment">
                                 <p class="headers">СПОСОБ ОПЛАТЫ</p>
                                 <ul>
-                                <li><input type="radio" name="payment" id="cash" value="Наличными при получении" checked>
+                                <li><input class="custom-radio" type="radio" name="payment" id="cash" value="Наличными при получении">
                                 <label for="cash">Наличными при получении</label></li>
-                                <li><input type="radio" name="payment" id="card" value="Картой">
+                                <li><input class="custom-radio" type="radio" name="payment" id="card" value="Картой">
                                 <label for="card">Картой</label></li>
                                 </ul>
                                 </div>
@@ -66,10 +57,43 @@ const ordering = {
                         </form>
                       </div>`)
             this.scroll()
+            this.choice()
         })
     },
     delivery() {
-
+        document.querySelector('.aboutRecipient').insertAdjacentHTML('beforeend',
+            `                  <section class="address">
+                                    <p class="aboutRecipient__text">Адрес получателя:</p>
+                                    <ul>
+                                    <li><input type="text" placeholder="Город" name="deliveryForm" id="city"></li>
+                                    <li><input type="text" placeholder="Улица" name="deliveryForm" id="street"></li>
+                                    <li><input type="text" placeholder="Дом" name="deliveryForm" id="houseNumber"></li>
+                                    <li><input type="text" placeholder="Квартира/офис" name="deliveryForm" id="apartment"></li>
+                                    </ul>
+                                `)
+    },
+    pickup() {
+        document.querySelector('.aboutRecipient').insertAdjacentHTML('beforeend',
+            `
+                                    <section class="our-address">
+                                    <p class="aboutRecipient__text">Пункт самовывоза:</p>
+                                    <ul>
+                                        <li>
+                                            <img src="../img/other/marker.svg" alt="marker">
+                                            <p>Москва, ул. Арбат, д. 29</p>
+                                        </li>
+                                        <li>
+                                            <p>Пн-пт с 9:00 до 20:00</p>
+                                        </li>
+                                        <li>
+                                            <p>+1 123 456 78 90</p>
+                                        </li>
+                                        <li>
+                                            <img src="" alt="">
+                                        </li>
+                                    </ul>
+                                    </section>
+                                `)
     },
     scroll() {
         let orderBlock = document.querySelector('.order');
@@ -78,5 +102,22 @@ const ordering = {
         orderBlock.style.height = '775px'
         orderBlock.style.overflowX = 'none'
         document.body.style.overflow = 'hidden'
+    },
+    choice() {
+        let choise = document.querySelectorAll('.custom-radio')
+        choise.forEach(item => item.addEventListener('click', (e) => {
+                if (!document.querySelector('.our-address') && document.getElementById('pickup').checked) {
+                    this.pickup()
+                        if (document.querySelector('.address')) {
+                            document.querySelector('.address').remove()
+                        }
+                } else if (!document.querySelector('.address') && document.getElementById('delivery').checked) {
+                    this.delivery()
+                        if (document.querySelector('.our-address')) {
+                            document.querySelector('.our-address').remove()
+                        }
+                }
+            }
+        ))
     }
 }
