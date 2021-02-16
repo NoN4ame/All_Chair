8936a61104233fabbb9c46bec7930c28b5d3611a
NoN4ame <Keir548@yandex.ru>
2021-02-16 22:51:49 +0300

    Сделал увеличение картинки через switch(сделано не правильно, картинки не того масштаба, надо переделать)

--- js/photoSlide.js
+++ js/photoSlide.js
@@ -1,28 +1,73 @@
-const gallery = []
+const gallery = [
+    {
+        project1: "../img/our_projects/big/project_1.png",
+        project2: "../img/our_projects/big/project_2.jpg",
+        project3: "../img/our_projects/big/project_3.jpg",
+        project4: "../img/our_projects/big/project_4.jpg",
+    }
+]
 const slider = {
-    init(){
+    init() {
         this.render()
     },
-    render(){
+    render() {
         let container = document.querySelector('.our-projects__content')
         let photo = container.querySelectorAll('.photo_container')
-        photo.forEach(item => item.addEventListener('click', (e) => {
-            this.backgroundStyle()
-            let src = item.querySelector('.photo').getAttribute('src')
-            document.querySelector('main').insertAdjacentHTML('beforeend', 
+        document.querySelectorAll('.photo').forEach(item => gallery.push(item))
+        photo.forEach(item => item.addEventListener('click', () => {
+            document.querySelector('main').insertAdjacentHTML('beforeend',
                 `<div class="invisible">
                         <div class="big_photo">
-                        <img src="${src}" alt="">
-</div>
+                            <img id="photoBig" src="" alt="">
+                        </div>
                 </div>`)
+            this.closeGallery()
+            this.openImg(item)
         }))
     },
-    backgroundStyle() {
-    document.querySelector('body').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
-    document.querySelector('header').style.filter = 'brightness(0.5)'
-    document.querySelectorAll('.container')
-        .forEach(div => div.style.filter = 'brightness(0.5)')
-    document.querySelectorAll('.container')[0].style.filter = 'none'
-},
+    styleCard() {
+        document.querySelector('body').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+        document.querySelector('header').style.filter = 'brightness(0.5)'
+        document.querySelectorAll('.container')
+            .forEach(div => div.style.filter = 'brightness(0.5)')
+        document.querySelectorAll('.container')[0].style.filter = 'none'
+    },
+    closeGallery() {
+        let closeBlock =  document.querySelector('.invisible')
+        closeBlock.addEventListener('click', (e) => {
+            if (e.target === closeBlock) {
+               this.delStyle()
+            }
+        })
+    },
+    delStyle () {
+        document.querySelector('.invisible').remove()
+        document.body.style.backgroundColor = 'transparent'
+        document.body.style.overflow = 'auto'
+        document.querySelector('header').style.filter = 'none'
+        document.querySelectorAll('.container')
+            .forEach(div => div.style.filter = 'none')
+    },
+    openImg(item){
+        let src = item.querySelector('.photo').getAttribute('alt')
+        switch (src) {
+            case 'project1' : if (src === 'project1')
+                console.log(gallery[0].project1)
+                document.getElementById('photoBig').src = gallery[0].project1
+                break
+            case 'project2' : if (src === 'project2')
+                console.log(gallery[0].project2)
+                document.getElementById('photoBig').src = gallery[0].project2
+                break
+            case 'project3' : if (src === 'project3')
+                console.log(gallery[0].project3)
+                document.getElementById('photoBig').src = gallery[0].project3
+                break
+            case 'project4' : if (src === 'project4')
+                console.log(gallery[0].project4)
+                document.getElementById('photoBig').src = gallery[0].project4
+                break
+        }
+    }
 }
 slider.init()
